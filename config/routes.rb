9ce57d4b289b21e5fad51498630cdeaf469598c2
Ben W <ben.wolthuis@gmail.com>
2023-06-29 01:07:13 +0200

    Cleaned up the list somewhat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   resource :home, only: [:index, :show]
-  resources :ranking, only: [:index, :show]
+  resources :ranking, only: [:index, :show] 
   get '/about', to: 'about#index'
 end
